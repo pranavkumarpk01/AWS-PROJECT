86b44dd70f321842ebf47cb8af96a8672171c64f
Ubuntu <ubuntu@ip-172-31-3-125.ap-south-1.compute.internal>
2024-05-09 17:50:52 +0000

    complete aws code

--- EmpApp.py
+++ EmpApp.py
@@ -1,13 +1,8 @@
 from flask import Flask, render_template, request
 from pymysql import connections
-import os
-import boto3
 from config import *
 
 app = Flask(__name__)
-
-bucket = custombucket
-region = customregion
 
 db_conn = connections.Connection(
     host=customhost,
@@ -15,7 +10,6 @@ db_conn = connections.Connection(
     user=customuser,
     password=custompass,
     db=customdb
-
 )
 output = {}
 table = 'employee'
@@ -38,48 +32,22 @@ def AddEmp():
     last_name = request.form['last_name']
     pri_skill = request.form['pri_skill']
     location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
 
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
-    if emp_image_file.filename == "":
-        return "Please select a file"
-
     try:
-
         cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
-
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
+    except Exception as e:
+        return str(e)
     finally:
         cursor.close()
 
-    print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
+
